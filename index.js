6bedac1045617200d25346b53d3a2a0f3ecf84a1
asv1997 <asvignesh1997@gmail.com>
2020-07-21 15:33:59 +0530

    changed the greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res,next) =>{
-    res.send({name: 'Vicky'})
+    res.send({name: 'Micky'})
 })
 
 const PORT = process.env.PORT || 5000;  // WE MUST USE THE PORT WHICH HEROKU GIVES US
